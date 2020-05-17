fb37e1ea0da973acfc34277ec7ac9b92a6574cb2
Sean <seanhb714@gmail.com>
2020-05-17 00:15:31 -0700

    com 5/15

--- app.js
+++ app.js
@@ -20,13 +20,60 @@
         getResults(searchbox.value);
       }
 
+      function onLoadAPI () {
+        fetch(`${api.base}weather?q=Rome&units=imperial&APPID=${api.key}`)
+          .then(weather => {
+            return weather.json();
+          }).then(displayResults);
+      }
+
       function getResults (query) {
         fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
           .then(weather => {
             return weather.json();
           }).then(displayResults);
       }
+    //   Change BG on weather type
+    //   function getType(weatherType) {
+    //     if (weatherType === 'Clear') {
+    //         console.log(rec)
+    //     }
+    //   }
+
+    function onLoadResults (weather) {
+        let city = document.querySelector('.location .city');
+        city.innerText = `${weather.name}`;
       
+        let now = new Date();
+        let date = document.querySelector('.location .date');
+        date.innerText = dateBuilder(now);
+      
+        let temp = document.querySelector('.current .temp');
+        temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+      
+        let icon = document.querySelector('.current .icon');
+        icon.innerHTML = `<img class ='icon'src='http://openweathermap.org/img/w/10d.png'/>`;//weather icon 
+
+        let weather_el = document.querySelector('.current .weather');
+        let weatherType = weather.weather[0].main;
+        weather_el.innerText = weatherType // type of weather 
+
+
+        
+        let hilow = document.querySelector('.hi-low');
+        hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
+      }
+      function dateBuilder (d) {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+      
+        return `${day} ${date} ${month} ${year}`;
+      }
       function displayResults (weather) {
         let city = document.querySelector('.location .city');
         city.innerText = `${weather.name}`;
@@ -38,15 +85,13 @@
         let temp = document.querySelector('.current .temp');
         temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
       
-        let type = document.querySelector('.type')
-        type.innerHTML = `<img src='http://openweathermap.org/img/wn/${weather.icon}@2x.png'/>` //weather icon 
         let weather_el = document.querySelector('.current .weather');
-        weather_el.innerText = weather.weather[0].main; // type of weather 
-      
+        let weatherType = weather.weather[0].main;
+        weather_el.innerText = weatherType // type of weather 
+
         let hilow = document.querySelector('.hi-low');
         hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
       }
-      
       function dateBuilder (d) {
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
